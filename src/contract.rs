b6f93fb50b58406e93f69d8cb2b6e670d23efcb3
Aoi1011 <ht.so.ik0102@gmail.com>
2022-12-09 16:47:32 +0700

    Create test case for transfer

--- src/contract.rs
+++ src/contract.rs
@@ -170,71 +170,103 @@ pub mod query {
 
 #[cfg(test)]
 mod tests {
+    use crate::contract::execute::transfer;
+    use crate::msg::ConfigResponse;
+
     use super::*;
     use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info};
-    use cosmwasm_std::{coins, from_binary};
+    use cosmwasm_std::{coins, from_binary, Addr, Attribute};
 
     #[test]
-    fn proper_initialization() {
+    fn test_proper_initialization() {
         let mut deps = mock_dependencies();
 
-        let msg = InstantiateMsg { count: 17 };
-        let info = mock_info("creator", &coins(1000, "earth"));
+        let msg = InstantiateMsg {
+            counter_offer: coins(40, "ETH"),
+            expires: 100_000,
+        };
+        let info = mock_info("creator", &coins(1, "BTC"));
 
         // we can just call .unwrap() to assert this was a success
         let res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
         assert_eq!(0, res.messages.len());
 
         // it worked, let's query the state
-        let res = query(deps.as_ref(), mock_env(), QueryMsg::GetCount {}).unwrap();
-        let value: GetCountResponse = from_binary(&res).unwrap();
-        assert_eq!(17, value.count);
+        let res = query(deps.as_ref(), mock_env(), QueryMsg::Config {}).unwrap();
+        let value: ConfigResponse = from_binary(&res).unwrap();
+        assert_eq!(100_000, value.expires);
+        assert_eq!("creator", value.owner.into_string());
+        assert_eq!("creator", value.creator.into_string());
+        assert_eq!(coins(1, "BTC"), value.collateral);
+        assert_eq!(coins(40, "ETH"), value.counter_offer);
     }
 
     #[test]
-    fn increment() {
+    fn test_transfer() {
         let mut deps = mock_dependencies();
 
-        let msg = InstantiateMsg { count: 17 };
-        let info = mock_info("creator", &coins(2, "token"));
-        let _res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
-
-        // beneficiary can release it
-        let info = mock_info("anyone", &coins(2, "token"));
-        let msg = ExecuteMsg::Increment {};
-        let _res = execute(deps.as_mut(), mock_env(), info, msg).unwrap();
-
-        // should increase counter by 1
-        let res = query(deps.as_ref(), mock_env(), QueryMsg::GetCount {}).unwrap();
-        let value: GetCountResponse = from_binary(&res).unwrap();
-        assert_eq!(18, value.count);
-    }
-
-    #[test]
-    fn reset() {
+        let msg = InstantiateMsg {
+            counter_offer: coins(40, "ETH"),
+            expires: 100_000,
+        };
+        let info = mock_info("creator", &coins(1, "BTC"));
         let mut deps = mock_dependencies();
 
-        let msg = InstantiateMsg { count: 17 };
-        let info = mock_info("creator", &coins(2, "token"));
-        let _res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
+        let res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
+        assert_eq!(res.messages.len(), 0);
 
-        // beneficiary can release it
-        let unauth_info = mock_info("anyone", &coins(2, "token"));
-        let msg = ExecuteMsg::Reset { count: 5 };
-        let res = execute(deps.as_mut(), mock_env(), unauth_info, msg);
-        match res {
-            Err(ContractError::Unauthorized {}) => {}
-            _ => panic!("Must return unauthorized error"),
+        // random cannot transfer
+        let info = mock_info("anyone", &[]);
+        let err = transfer(deps.as_mut(), info, Addr::unchecked("anyone")).unwrap_err();
+        match err {
+            ContractError::Unauthorized { .. } => {}
+            e => panic!("unexpected error: {}", e),
         }
 
-        // only the original creator can reset the counter
-        let auth_info = mock_info("creator", &coins(2, "token"));
-        let msg = ExecuteMsg::Reset { count: 5 };
-        let _res = execute(deps.as_mut(), mock_env(), auth_info, msg).unwrap();
+        // owner can transfer
+        let info = mock_info("creator", &[]);
+        let res = transfer(deps.as_mut(), info, Addr::unchecked("someone")).unwrap();
+        assert_eq!(res.attributes.len(), 2);
+        assert_eq!(
+            res.attributes[0],
+            Attribute {
+                key: "action".to_string(),
+                value: "transfer".to_string()
+            }
+        );
 
-        // should now be 5
-        let res = query(deps.as_ref(), mock_env(), QueryMsg::GetCount {}).unwrap();
-        let value: GetCountResponse = from_binary(&res).unwrap();
-        assert_eq!(5, value.count);
+        // check updated properly
+        let res = query(deps.as_ref(), mock_env(), QueryMsg::Config {}).unwrap();
+        let value: ConfigResponse = from_binary(&res).unwrap();
+        assert_eq!("someone", value.owner.as_str());
+        assert_eq!("creator", value.creator.as_str());
     }
+
+    // #[test]
+    // fn reset() {
+    //     let mut deps = mock_dependencies();
+
+    //     let msg = InstantiateMsg { count: 17 };
+    //     let info = mock_info("creator", &coins(2, "token"));
+    //     let _res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
+
+    //     // beneficiary can release it
+    //     let unauth_info = mock_info("anyone", &coins(2, "token"));
+    //     let msg = ExecuteMsg::Reset { count: 5 };
+    //     let res = execute(deps.as_mut(), mock_env(), unauth_info, msg);
+    //     match res {
+    //         Err(ContractError::Unauthorized {}) => {}
+    //         _ => panic!("Must return unauthorized error"),
+    //     }
+
+    //     // only the original creator can reset the counter
+    //     let auth_info = mock_info("creator", &coins(2, "token"));
+    //     let msg = ExecuteMsg::Reset { count: 5 };
+    //     let _res = execute(deps.as_mut(), mock_env(), auth_info, msg).unwrap();
+
+    //     // should now be 5
+    //     let res = query(deps.as_ref(), mock_env(), QueryMsg::GetCount {}).unwrap();
+    //     let value: GetCountResponse = from_binary(&res).unwrap();
+    //     assert_eq!(5, value.count);
+    // }
 }
