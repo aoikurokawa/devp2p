f971a189f412dcfe88e12af8d23273b3df01b33c
Aoi1011 <ht.so.ik0102@gmail.com>
2023-01-26 23:20:37 +0700

    Init new project

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,14 @@
-pub mod contract;
-mod error;
-pub mod helpers;
-pub mod integration_tests;
-pub mod msg;
-pub mod state;
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
 
-pub use crate::error::ContractError;
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
